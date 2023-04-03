fc52e397193f789c53c5dc50a2ec6155abb15de2
Hwanie91 <jh27hot@gmail.com>
2023-04-03 15:02:27 +0900

    Update Exam.java

--- src/part3/ex3/Getters와Setters/Exam.java
+++ src/part3/ex3/Getters와Setters/Exam.java
@@ -5,6 +5,15 @@ public class Exam {
     int eng;
     int math;
 
+    public Exam() {
+        this(0, 0, 0);
+    }
+    public Exam(int kor, int eng, int math) {
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+    }
+
     public int getKor() {
         return kor;
     }
@@ -27,5 +36,13 @@ public class Exam {
 
     public void setMath(int math) {
         this.math = math;
+    }
+
+    public int total() {
+        return kor + eng + math;
+    }
+
+    public float avg() {
+        return total()/3.0f;
     }
 }
